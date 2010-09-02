5c3d36ec3625c847a7c64bdb4d3bb0acf8a3000e
Philip Transfeld <p.transfeld@sap.com>
2010-09-02 10:06:08 -0400

    volunteering overview ui

--- omega/app/helpers/z_favorites_helper.rb
+++ omega/app/helpers/z_favorites_helper.rb
@@ -1,7 +1,9 @@
 # prefix with Z to load after Trams::Favorites::FavoritesHelper so we can override these methods
 module ZFavoritesHelper
   def add_to_favorites(model)
-    link_to('Add', favorite_path(model))
+    link_to(favorite_path(model)) do
+      '<img src="/images/icons/heart-plus.png" alt="">Add to Favorites'
+    end
   end
 
   def remove_from_favorites(model)
