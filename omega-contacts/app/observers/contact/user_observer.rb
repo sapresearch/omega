f5236cfe6b0d5dfc7e101c48f06da4e31104e4a0
Samuel Kadolph <samuel@kadolph.com>
2010-09-02 10:55:35 -0400

    Finished ContactObserver to create a new Contact for each new User

--- omega-contacts/app/observers/contact/user_observer.rb
+++ omega-contacts/app/observers/contact/user_observer.rb
@@ -2,6 +2,10 @@ class Contact::UserObserver < ActiveRecord::Observer
   observe :user
 
   def after_update(user)
-    puts "after_update(#{user})"
+    unless Contact.for(user)
+      Contact.new do |c|
+        c.user = user
+      end.save(:validate => false)
+    end
   end
 end
