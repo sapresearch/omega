a190e420e7e2d6f173befd3a0d8f8924927c0431
Philip Transfeld <p.transfeld@sap.com>
2010-09-17 17:50:19 -0400

    home layout, footer,marketing footer

--- omega/app/controllers/home_controller.rb
+++ omega/app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   respond_to :html
 
   def index
-    
+    @positions =  Volunteering::Position.limit(5)
+    @events = Event.limit(6)
+    respond_with(@positions, @events)
   end
 end
