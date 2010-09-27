43767ffaa9f4f6b455d080d2e277624163f1e118
Samuel Kadolph <samuel@kadolph.com>
2010-09-27 09:34:06 -0400

    Added custom date format for the iCalendar format which views/events/index.ics now uses.

--- omega-calendar/lib/omega/calendar/tram.rb
+++ omega-calendar/lib/omega/calendar/tram.rb
@@ -2,6 +2,10 @@ module Omega
   module Calendar
     class Tram < Trams::Base
       config.omega.calendar = ActiveSupport::OrderedOptions.new
+
+      initializer :'omega.calendar.add_icalendar_date_format' do
+        Time::DATE_FORMATS[:icalendar] = '%Y%m%dT%H%M%SU'
+      end
     end
   end
 end
