53c03768cba24066327c8ab58c9292603da70b02
Samuel Kadolph <samuel@kadolph.com>
2010-09-29 14:43:14 -0400

    Mime::ICS already exists so it shouldn't be registered in Omega::Calendar::Tram.

--- omega-calendar/lib/omega/calendar/tram.rb
+++ omega-calendar/lib/omega/calendar/tram.rb
@@ -3,10 +3,6 @@ module Omega
     class Tram < Trams::Base
       config.omega.calendar = ActiveSupport::OrderedOptions.new
 
-      initializer :'omega.calendar.add_mime_types' do
-        Mime::Type.register_alias('text/plain', :ics)
-      end
-
       initializer :'omega.calendar.add_icalendar_date_format' do
         Time::DATE_FORMATS[:icalendar] = '%Y%m%dT%H%M%SU'
       end
