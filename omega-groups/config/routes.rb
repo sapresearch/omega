2cff3184158dba09c062d4b2c8bc49039118f643
Samuel Kadolph <samuel@kadolph.com>
2010-09-29 14:54:44 -0400

    Fixed routing.

--- omega-groups/config/routes.rb
+++ omega-groups/config/routes.rb
@@ -1,21 +1,5 @@
 Omega::Groups::Tram.routes.draw do
-  scope :path => 'groups', :as => 'groups', :module => 'groups' do
-    resources :groups
-    resources :households
-    resources :organizations
-    resources :skills
-    resources :interests
-  end
-
   resources :groups do
-    collection do
-     
-    end
-
-
     
   end
-
-  match 'contacts/:action' => 'groups#search'
-
-  end
+end
