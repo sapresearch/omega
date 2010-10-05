724f3899a81a72251b9ed577786f177be15beaba
Philip Transfeld <p.transfeld@sap.com>
2010-10-05 14:13:37 -0400

    volunteering column fix

--- omega-volunteering/db/migrate/20101005112600_add_column_action.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnAction < ActiveRecord::Migration
-    def self.up
-      add_column :volunteering_records, :action, :string
-    end
-
-    def self.down
-      remove_column :volunteering_records, :action
-    end
-  end
