67d1a43df7d34e45a3395fbbc87268829e4254da
Philip Transfeld <p.transfeld@sap.com>
2010-10-12 17:17:25 -0400

    samules helper

--- omega-volunteering/app/models/volunteering/record.rb
+++ omega-volunteering/app/models/volunteering/record.rb
@@ -3,7 +3,7 @@ class Volunteering::Record < Omega::Model
   MAX_RECORDS_PER_PAGE = 5
 
   belongs_to  :position
-  belongs_to  :contact
+  belongs_to  :contact, :validate => false
   has_many    :time_entries
 
   default_scope order('created_at desc')
--- omega/app/helpers/permissions_helper.rb
+++ omega/app/helpers/permissions_helper.rb
@@ -1,5 +1,5 @@
 module PermissionsHelper
-  def with_permissions(permission, &block)
+  def with_permission(permission, &block)
     if current_user.has_permission?(permission)
       capture(&block)
     end
