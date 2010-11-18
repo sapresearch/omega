df10eb729162ed12aa174d3d70a8fb95db26590b
Philip Transfeld <p.transfeld@sap.com>
2010-11-18 10:30:53 -0500

    Fix merge

--- omega-groups/public/javascripts/omega_threads.js
+++ omega-groups/public/javascripts/omega_threads.js
@@ -1,17 +1,13 @@
 $(function(){
     $('#threads-list').find('span.ui-icon-search ').hover(
                                                          function(){
-
-<<<<<<< HEAD
                                                          $(this).siblings('div.thred-preview').show();
                                                          },
                                                          function(){
                                                              $(this).siblings('div.thred-preview').hide();
-=======
                                                          $(this).next('.thred-preview').show();
                                                          },
                                                          function(){
                                                              $(this).next('.thred-preview').hide();
->>>>>>> f230a58... update imports
                                                          })
 });
