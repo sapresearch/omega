d72eaf7567f493e059fce34f89ab8a2adc8d5c32
Philip Transfeld <p.transfeld@sap.com>
2010-11-24 15:57:27 -0500

    layout fixes for volunteering tables

--- omega-volunteering/public/javascripts/new_time_entry.js
+++ omega-volunteering/public/javascripts/new_time_entry.js
@@ -6,8 +6,8 @@
  * To change this template use File | Settings | File Templates.
  */
 
-$(document).ready(function() {
-    $('#sidebar-date-picker').datepicker({
+$(function() {
+    $('.datepickr').datepicker({
         firstDay:1,
         beforeShowDay: function(date) {
             var a = new Array();
@@ -17,11 +17,29 @@ $(document).ready(function() {
             return a;
         }, dateFormat: 'yy-mm-dd',
         onSelect: function(dateText) {
-        $('#volunteering_time_entry_week, #cw').val(dateText);
+        $(this).val(dateText);
         },
          weekHeader: 'W' ,
         showWeek: true 
 
+    });
+
+        $('#new_volunteering_time_entry').find('span.increase').click(function() {
+        var ipt = $(this).next('input');
+        if (ipt.val() =='') {
+
+            ipt.val(1)
+        } else {
+            var curval = parseFloat(ipt.val());
+            ipt.val(curval + 1)
+        }
+
+
+    });
+    $('#new_volunteering_time_entry').find('span.decrease').click(function() {
+        var ipt = $(this).prev('input');
+        var curval = parseFloat(ipt.val());
+        if (curval > 1) ipt.val(curval - 1);
     });
 
 });
