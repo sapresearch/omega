f4fc3055192b9ac17ef704599ea65a79c06a47b5
Noorulain <ann.khurshid@gmail.com>
2010-11-29 13:31:09 -0500

    undo import using filters

--- omega-contacts/public/javascripts/import.js
+++ omega-contacts/public/javascripts/import.js
@@ -1,19 +1,27 @@
 $(function() {
 
-    $('#import_date').click(function() {
+    $('#import_filter_By_Mapping').click(function() {
+        import_data(this.id)
+    });
 
+    $('#import_filter_By_Data').click(function() {
+        import_data(this.id)
+    });
 
-         $.ajax({
+});
+
+function import_data(selection){
+
+    var import_date = document.getElementById('import_date').value;
+
+    $.ajax({
 
                 type: "GET",
                 url: "/contacts/imports/get_import_data",
-                data: "created_at="+this.value,
+                data: { "created_at": import_date, "filter": selection },
                 success: function(html){
-                    $('#imported_contacts').html(html);
+
+                    $('#import_contacts').html(html);
                 }
-       });
-
-
     });
-});
-
+}
