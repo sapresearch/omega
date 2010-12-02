724780c137fc0d821474ac33ca52a4b763bf1b05
Noorulain <ann.khurshid@gmail.com>
2010-12-02 14:16:42 -0500

    upload content-type test

--- omega-contacts/app/models/contact/import.rb
+++ omega-contacts/app/models/contact/import.rb
@@ -6,7 +6,7 @@ class Contact::Import < Omega::Model
 
   validates_attachment_presence :csv
 
-  validates_attachment_content_type :csv, :content_type => ['text/plain', 'text/csv', 'applications/csv', 'applications/vnd.ms-excel' ]
+  validates_attachment_content_type :csv, :content_type => ['text/plain', 'application/vnd.ms-excel' ]
 
 
 end
