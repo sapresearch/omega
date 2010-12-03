fcc5573418799493eb8c113abe5124e1fac08c8c
Philip Transfeld <p.transfeld@sap.com>
2010-12-03 11:35:37 -0500

    import wizard bug fix for step wizard helper text

--- omega-contacts/public/javascripts/import.js
+++ omega-contacts/public/javascripts/import.js
@@ -7,30 +7,36 @@ $(function() {
     $('#import_filter_By_Data').click(function() {
         import_data(this.id)
     });
+    
 
     $('#wizard-intro li').hover(function(){
       
         $(this).find('.wizard-explanation').show().animate({
-                             marginTop: '-110px', /* The next 4 lines will vertically align this image */
+marginTop: '-110px', /* The next 4 lines will vertically align this image */
                              marginLeft: '-110px',
                              top: '50%',
                              left: '50%',
                              width: '200px', /* Set new width */
-                             height: '200px', /* Set new height */
-                             padding: '10px'
-                         }, 200).addClass('box-shadow-dark');
+                             height: '200px' /* Set new height */
+
+                         }, 200)
     },function(){
+        
         $(this).find('.wizard-explanation').animate({
-                             marginTop: '0', /* Set alignment back to default */
+ marginTop: '0', /* Set alignment back to default */
                              marginLeft: '0',
                              top: '0',
                              left: '0',
                              width: '200px', /* Set width back to default */
-                             height: '200px' /* Set height back to default */
+                             height: '0px' /* Set height back to default */
                              
-                         }, 400,function(){
-          $(this).hide().removeClass('box-shadow-dark');  
+                         },200, function(){
+            
+        $(this).hide()
         })
+
+
+
     })
 
 });
