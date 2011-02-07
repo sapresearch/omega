63a993b8f8cf27c8435f12cc855ccc02afa41741
Noorulain <ann.khurshid@gmail.com>
2011-02-07 09:28:24 -0500

    newsletters

--- omega-contacts/db/migrate/20100609191813_create_contacts.rb
+++ omega-contacts/db/migrate/20100609191813_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration
   def self.up
     create_table :contacts do |t|
-      t.references :user,       :null => true
+      t.references :user, :null => true
       t.string     :title
       t.string     :first_name
       t.string     :last_name
@@ -18,7 +18,6 @@ class CreateContacts < ActiveRecord::Migration
       t.string     :about_us
       t.string     :status
       
-      
       t.boolean    :evergreen_news
       t.boolean    :evergreen_bw_news
       t.boolean    :gta_volunteer_news
@@ -29,7 +28,6 @@ class CreateContacts < ActiveRecord::Migration
       t.boolean    :evergreen_funding_opp
       t.boolean    :evergreen_bw_school_program
       t.boolean    :evergreen_bw_family_program
-      
       
       t.boolean    :do_not_email
       t.boolean    :no_bulk_emails
