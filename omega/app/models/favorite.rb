1370488603320907d7d5d587dd81d70bc32bb855
Samuel Kadolph <samuel@kadolph.com>
2011-03-07 15:10:35 -0500

    Fix problem with Favorite.for scope

--- omega/app/models/favorite.rb
+++ omega/app/models/favorite.rb
@@ -3,7 +3,7 @@ class Favorite < ActiveRecord::Base
   belongs_to :item, :polymorphic => true
 
   scope :of, lambda { |user| where('user_id = ?', user) }
-  scope :klassed, lambda { |klass| where('item_type = ?', klass)}
+  scope :klassed, lambda { |klass| where('item_type = ?', klass.to_s) }
   scope :for, lambda { |model| where('item_id = ?', model).klassed(model.class) }
 
   validates :item_id, :uniqueness => { :scope => [:user_id, :item_id, :item_type] }
