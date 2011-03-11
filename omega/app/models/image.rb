4adaabda5873b35459167a812b867f51c148297e
Noorulain <ann.khurshid@gmail.com>
2011-03-11 14:11:42 -0500

    logo size

--- omega/app/models/image.rb
+++ omega/app/models/image.rb
@@ -3,7 +3,7 @@ class Image < Omega::Model
   has_attached_file :image,
                     :url => "/images/application/logo.png",
                     :path => ":rails_root/public/images/application/logo.png",
-                    :styles => { :small => "260x60!", :large => "260x90!" }
+                    :styles => { :small => "260x60!", :large => "260x160!" }
 
   validates_attachment_content_type :image, :content_type => ['image/png', 'image/x-png', 'image/jpg', 'image/gif', 'image/jpeg' ]
   
