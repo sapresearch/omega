bb1f8ff6ea5687108eac8300164075da5d6726a6
Noorulain <ann.khurshid@gmail.com>
2011-03-28 12:21:13 -0400

    datepicker fix

--- omega-volunteering/public/javascripts/new_volunteering_position.js
+++ omega-volunteering/public/javascripts/new_volunteering_position.js
@@ -6,6 +6,7 @@
 $(function() {
 
     $('.tpickr').timepicker();
+    $('.datepickr').datepicker({minDate: 0});
 
     /* jqueryui datepicker defaults */
     $('#volunteer_position_form').find(".datepickr").datepicker({ minDate: 0 });
