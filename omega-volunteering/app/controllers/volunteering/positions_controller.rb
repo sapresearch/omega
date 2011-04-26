cc3b0fceb08f9fb5a98e6f928785f9754b495580
Noorulain <ann.khurshid@gmail.com>
2011-04-26 11:34:43 -0400

    delete

--- omega-volunteering/app/controllers/volunteering/positions_controller.rb
+++ omega-volunteering/app/controllers/volunteering/positions_controller.rb
@@ -64,7 +64,7 @@ class Volunteering::PositionsController < Omega::Controller
   def destroy
     @position = Volunteering::Position.find(params[:id])
     @position.destroy
-    respond_with(@position)
+    redirect_to volunteering_positions_url
   end
 
   def skills
