463900e33ae02e953fdedcdcb887b269297c9117
Kexia Tang <kexia.tang@sap.com>
2011-06-30 15:59:47 -0400

    service details is shown

--- omega-services/app/helpers/services_helper.rb
+++ omega-services/app/helpers/services_helper.rb
@@ -4,15 +4,6 @@ module ServicesHelper
   def is_admin?(user)
     true
   end
-=begin
-  def detail_field_values(service)
-    j = ActiveSupport::JSON
-    field_values = j.decode(service.detail_field_values)
-    html = ""
-    field_values.each do |key, value|
 
-    end
-  end
-=end
 end
 
