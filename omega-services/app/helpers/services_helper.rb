8feee9130851a8ee9d450c789f0d5b10dd2ae47e
Kexia Tang <kexia.tang@sap.com>
2011-07-04 10:07:20 -0400

    ajax breadcrumb

--- omega-services/app/helpers/services_helper.rb
+++ omega-services/app/helpers/services_helper.rb
@@ -7,7 +7,7 @@ module ServicesHelper
 
   def bread_crumb(service)
     while not service.nil?
-      html = " > ".html_safe + (link_to service.name, services_url(:super_service_id=>service.id)) + html
+      html = " > ".html_safe + (link_to service.name, services_url(:super_service_id=>service.id), :remote=>true) + html
       service = service.super_service
     end
     html = (link_to "All services", services_url(:super_service_id=>"root")) + html;
