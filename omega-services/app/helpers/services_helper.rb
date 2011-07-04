bf198936b3f088b60f93dc7dc44b91a0028c0d82
Kexia Tang <kexia.tang@sap.com>
2011-07-04 09:49:05 -0400

    breadcrumb for services list added

--- omega-services/app/helpers/services_helper.rb
+++ omega-services/app/helpers/services_helper.rb
@@ -5,5 +5,15 @@ module ServicesHelper
     true
   end
 
+  def bread_crumb(service)
+    while not service.nil?
+      html = " > ".html_safe + (link_to service.name, services_url(:super_service_id=>service.id)) + html
+      service = service.super_service
+    end
+    html = (link_to "All services", services_url(:super_service_id=>"root")) + html;
+    html = "<div class='bread_crumb'>".html_safe + html
+    html += "</div>".html_safe
+  end
+
 end
 
