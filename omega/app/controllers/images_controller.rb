fabcf25566010f48f54903cd57ae0d2ada6c5161
Paul Miller <I829287@ymqdomega1.ymq.sap.corp>
2011-08-08 12:21:07 -0400

    Changed edit_logo so you can't edit the banner

--- omega/app/controllers/images_controller.rb
+++ omega/app/controllers/images_controller.rb
@@ -10,9 +10,12 @@ class ImagesController < Omega::Controller
   def create
 		id = params[:image][:id]
 		banner_or_logo = params[:image][:banner_or_logo]
+		
+		###############################################	
+		banner_or_logo = "Logo" # This is just while you are only updating the logo only. Change this if you allow users' to updat ebanner as well.
+		###############################################	
 
 		# Edit an existing image.
-		#if (!Image.find(id).nil?) then
 		if (!id.nil?) then
 			Image.update_all({:image_in_use => TRUE}, {:id => id})
 			Image.update_all({:banner_or_logo => banner_or_logo}, {:id => id})
@@ -22,6 +25,11 @@ class ImagesController < Omega::Controller
     	@image = Image.create(params[:image])
 			id = @image.id
 			Image.update_all({:image_in_use => TRUE}, {:id => id})
+
+			###############################################	
+			Image.update_all({:banner_or_logo => banner_or_logo}, {:id => id}) # This is just while you are only updating the logo only. Change this if you allow users' to updat ebanner as well.
+			###############################################	
+
 		end
 
 		Image.where("id != ?", id).where("banner_or_logo = ?", banner_or_logo).update_all(:image_in_use => FALSE)
