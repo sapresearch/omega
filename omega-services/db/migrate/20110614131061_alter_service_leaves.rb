0cb3e89808c433df5f4c15204147205a42ebbce4
Kexia Tang <kexia.tang@sap.com>
2011-09-08 16:08:16 -0400

    fix

--- omega-services/db/migrate/20110614131061_alter_service_leaves.rb
+++ omega-services/db/migrate/20110614131061_alter_service_leaves.rb
@@ -1,7 +1,6 @@
 class AlterServiceLeaves < ActiveRecord::Migration
   def self.up
     rename_column :service_leaves, :type, :register_type
-    ServiceLeaf.all.each{|sl|sl.update_attribute(:register_type, "enrollable") if sl.register_type.nil?} # fill the empty fields.
   end
 
   def self.down
