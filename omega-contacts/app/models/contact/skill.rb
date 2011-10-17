c1caecebe294f3c67c034df2d8707b05a28d2145
Paul Miller <paul.miller01@sap.corp>
2011-10-17 10:49:50 -0400

    Layout in volunteering/positions.

--- omega-contacts/app/models/contact/skill.rb
+++ omega-contacts/app/models/contact/skill.rb
@@ -1,6 +1,6 @@
 class Contact::Skill < Omega::Model
-  has_and_belongs_to_many :contacts, :join_table => 'contact_contacts_skills'
-  has_and_belongs_to_many :users, :join_table => 'contact_skills_users'
+	has_and_belongs_to_many :contacts, :join_table => 'contact_contacts_skills'
+	has_and_belongs_to_many :users, :join_table => 'contact_skills_users'
 	attr_accessible :name
 
   validates :name, :presence => true,
