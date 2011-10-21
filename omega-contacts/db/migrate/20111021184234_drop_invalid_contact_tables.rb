958c7383199167e4afb0d3272f8128d027987d0f
Paul Miller <paul.miller01@sap.corp>
2011-10-21 15:25:11 -0400

    Drop invalid contact_contacts_values

---
+++ omega-contacts/db/migrate/20111021184234_drop_invalid_contact_tables.rb
@@ -0,0 +1,16 @@
+class DropInvalidContactTables < ActiveRecord::Migration
+
+  TABLES = %w{contact_contacts_values}
+              
+  def self.up
+    TABLES.each do |table|
+      drop_table table if table_exists?(table)
+    end
+  end
+
+  def self.down
+    TABLES.each do |table|
+      create_table table unless table_exists?(table)
+    end
+  end
+end
