7581708298617125a8c7b4c52a271a921cca55e6
Paul Miller <paul.miller01@sap.corp>
2011-10-24 15:37:34 -0400

    Test

--- omega/app/controllers/users_controller.rb
+++ omega/app/controllers/users_controller.rb
@@ -150,15 +150,14 @@ class UsersController < Omega::Controller
 		if !current_user.is_admin?
 			registered_services = ServiceRegistration.filter_services_by_registrant(Service.all, @contact)
 			if not registered_services.empty?
-				@service_events = registered_services.inject(Array.new) do |service_events, service|
+				@service_events = Array.new
+				registered_services.each do |service|
 					registrations = service.service_registrations.select { |sr| sr.registrant == @contact }
 					if !registrations.at(0).nil?
 						status = registrations.at(0).status
 						next_event = service.next_event.nil? ? "TBD" : service.next_event.start_at.to_s.gsub(/:00 .*/, "")
 						@is_nil = @is_nil == false ? @is_nil : service_events.nil?
-						if service_events.respond_to? :<<
-							service_events << { :service => service, :next_event => next_event, :status => status }
-						end
+						@service_events << { :service => service, :next_event => next_event, :status => status }
 					end
 				end 
 			end 
