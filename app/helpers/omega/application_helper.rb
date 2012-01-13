bf8f5987fcba2e2c323e933a809ebeb1c1d865a6
Paul Miller <paul.miller01@sap.corp>
2012-01-13 14:21:00 -0500

    Moved image selection for the logo from a helper to CSS file. Renamed application_evergreen as application. Cleaned up home page layout.

--- app/helpers/omega/application_helper.rb
+++ app/helpers/omega/application_helper.rb
@@ -17,27 +17,5 @@ module Omega
 	  	}
 	  end
 	  
-		# Returns the url for the banner or logo. The url is used in the background-image, defined in an inline style definition in omega/app/views/layouts/application.html.erb
-		def image_in_use_url(banner_or_logo)
-			image = Image.all.select{ |i| i.image_in_use and i.banner_or_logo == banner_or_logo }.at(0)
-			url = image.image.url if image.respond_to? 'image'
-			if url.nil? and banner_or_logo == "Banner" then
-				return "../images/pixel_shim.png"
-			elsif url.nil? and banner_or_logo == "Logo" then
-				return "../images/application/logo.png"
-			else
-				return url
-			end
-		end
-	
-		def banner_in_use
-			url = image_in_use_url("Banner")
-			return "<div id=\"logo\" style=\"background:url(" + url + ") no-repeat left scroll transparent\" ><\/div>"
-		end
-	
-		def logo_in_use
-			url = image_in_use_url("Logo")
-			"<div id=\"logo\" style=\"background:url(" + url + ") no-repeat left scroll transparent\" ></div>"
-		end
   end
 end
