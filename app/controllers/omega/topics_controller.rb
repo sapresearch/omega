ac2df942b369ca8fe5d6629bbaff5c525b8de279
Paul Miller <paul.miller01@sap.corp>
2012-01-19 15:29:18 -0500

    Copied 7 edited controllers from evergreen to master.

---
+++ app/controllers/omega/topics_controller.rb
@@ -0,0 +1,22 @@
+class TopicsController < Omega::Controller
+
+  require_dependency "application_lib.rb"
+  include ApplicationLib
+  
+  respond_to :html, :xml, :json, :js
+
+  def show
+    @group = Group.find(params[:group_id])
+    @topic = Topic.find(params[:id])
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @topic = Topic.find(params[:id])
+    @topic.post.destroy
+    
+    render :js => "window.location = '#{space_group_url(@group)}'"
+    return
+  end
+
+end
