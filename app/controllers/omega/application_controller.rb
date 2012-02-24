f0ef8c728970e32495439bd280e44f481fcba2e2
Paul Miller <paul.miller01@sap.com>
2012-02-24 13:35:03 -0500

    Testing model autoloads.

--- app/controllers/omega/application_controller.rb
+++ app/controllers/omega/application_controller.rb
@@ -1,5 +1,7 @@
 module Omega
   class ApplicationController < ActionController::Base
 	puts "\n In Omega::ApplicationController in Omega\n"
+	puts "\n In Omega::ApplicationController in Omega. Defined: #{defined?(Omega::Volunteering::Position)}"
+	puts "\n In Omega::ApplicationController in Omega. Defined volunteering: #{defined?(Omega::Volunteering)}"
   end
 end
--- app/controllers/omega/home_controller.rb
+++ app/controllers/omega/home_controller.rb
@@ -2,10 +2,15 @@ module Omega
 	class HomeController < Omega::Controller
 	  respond_to :html
 		puts "\n In Omega::HomeController in Omega\n"
+		puts "\n In Omega::HomeController in Omega. Defined: #{defined?(Omega::Volunteering::Position)}"
+		puts "\n In Omega::HomeController in Omega. Defined volunteering: #{defined?(Omega::Volunteering)}"
 	
 	  def index
 			puts "\n In Omega::HomeController#index in Omega\n"
-	    @positions =  Volunteering::Position.limit(5)
+			puts "\n In Omega::HomeController#index in Omega. Defined: #{defined?(Omega::Volunteering::Position)}"
+			puts "\n In Omega::HomeController in Omega#index. Defined volunteering: #{defined?(Omega::Volunteering)}"
+	    #@positions =  Volunteering::Position.limit(5)
+	    @positions = []
 	    # @services = Service.limit(5) # original
 		 # just for testing meta.
 		 #@services = Service.real_public_service_leaves.reverse.take(5)
