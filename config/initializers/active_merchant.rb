00181bb8b87e77a51202d66187e96687f52ec272
Paul Miller <paul.miller01@sap.com>
2012-03-05 16:08:49 -0500

    Can't load Omega::Setting in active_merchant.rb. Commented it out to test in production mode.

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,8 +1,10 @@
 require "active_merchant"
 
-if Rails.env.production?
-  PAYPAL_ACCOUNT = Setting.first.user.email
-else
+# TODO commented this out to test in production.
+# Can't seem to require Omega::Setting in this file.
+#if Rails.env.production?
+#  PAYPAL_ACCOUNT = Setting.first.user.email
+#else
   PAYPAL_ACCOUNT = 'tang_1324417292_biz@gmail.com'
   ActiveMerchant::Billing::Base.mode = :test
-end
+#end
