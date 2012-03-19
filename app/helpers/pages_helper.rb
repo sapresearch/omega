ba6e1cf47d31bc509bea856ce32d74693bf8433e
Paul Miller <paul.miller01@sap.corp>
2012-03-19 12:05:53 -0400

    Prefixed all '_url' and '_path' helpers with 'tenant_'

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,9 +1,9 @@
 	module PagesHelper
-	  def current_page_path
+	  def tenant_current_page_path
 	    current = url_for(params)
 	    current.gsub!(/\A\//, '')
 	    current = 'home' if current.empty?
 	
-	    page_path(current)
+	    tenant_page_path(current)
 	  end
 	end
