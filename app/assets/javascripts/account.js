98897d93819ad6033e861c6bdc869280a32172cc
Kexia Tang <kexia.tang@sap.com>
2012-04-30 17:01:44 -0400

    fix

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -1,3 +1,8 @@
+//= require thirdParty/jquery
+//= require thirdParty/jquery-ui
+//= require rails
+//= require thirdParty/jquery.tipsy
+
 $(function(){
     $('.operation_button').button({ icons: {primary:'ui-icon-gear'} });
 })
