c1d673e5dd1bed103a6b48dc95a1045ef3c0ed37
Kexia Tang <kexia.tang@sap.com>
2012-07-12 16:11:04 -0400

    fix deleting contact function

--- app/assets/javascripts/omega_contacts.js
+++ app/assets/javascripts/omega_contacts.js
@@ -153,7 +153,7 @@ function update_contacts(contacts, group_id, group_name) {
                     + contacts[i].first_name + '</a>'
                     + '<td class="text-right" width="40px"><span class="mgm-contact hide">'
                     + '<span data-tooltip="Add me to another group me by dragging into a group on the left" class="om-icon-only om-blue-icon ui-icon-arrow-4-diag"></span>'
-                    + '<a  data-remote="true" data-method="delete" href="/contacts/' + contacts[i].id + '" class="remove-contact">'
+                    + '<a  data-remote="true" data-method="delete" href="'+account_prefix_path+'/contacts/' + contacts[i].id + '" class="remove-contact">'
                     + '<span class="om-icon-only om-blue-icon ui-icon-trash" data-tooltip="Delete this contact from the database"></span></a>'
                     + '</span></td>'
                     + '</td></tr>';
